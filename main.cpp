0fa13a11033da8dce09fab6836aa1d4389f8d44f
flwrr <flwrr@LAPTOP-RLOLD55G.>
2025-05-16 14:01:28 -1000

    coloring sphere by normal

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@
 #include <iostream>
 
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+// bool hit_sphere(const point3& center, double radius, const ray& r) {
+//     // x^2 + y^2 + z^2 = r^2    -   xyz is on sphere surface
+//     // x^2 + y^2 + z^2 < r^2    -   xyz is inside sphere
+//     // x^2 + y^2 + z^2 > r^2    -   xyz is outside sphere
+//     vec3 oc = center - r.origin();
+//     auto a = dot(r.direction(), r.direction());
+//     auto b = -2.0 * dot(r.direction(), oc);
+//     auto c = dot(oc, oc) - radius*radius;
+//     auto discriminant = b*b - 4*a*c;
+//     return (discriminant >= 0);
+//     // discriminant of 0 or higher implies an intersection
+// }
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
     // x^2 + y^2 + z^2 = r^2    -   xyz is on sphere surface
     // x^2 + y^2 + z^2 < r^2    -   xyz is inside sphere
     // x^2 + y^2 + z^2 > r^2    -   xyz is outside sphere
@@ -13,15 +26,24 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
     // discriminant of 0 or higher implies an intersection
+    auto discriminant = b*b - 4*a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 
+
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r)) {
-        return color(1,0,0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    // point t at which point ray r hits the sphere  
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
