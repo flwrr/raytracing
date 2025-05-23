a568cdde6a54f1972b09277235cc12baff4bbf0a
flwrr <cfritchie.hi@gmail.com>
2025-05-23 12:11:19 -1000

    defocus blur using thin lens approximation

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@
 
 int main() {
     hittable_list world;
-
-    auto R = std::cos(pi/4);
-
+    
     // ------------ viewport test world -------------
+    // auto R = std::cos(pi/4);
     // auto material_left = make_shared<lambertian>(color(0,0,1));
     // auto material_right = make_shared<lambertian>(color(1,0,0));
     // world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
@@ -23,66 +22,75 @@ int main() {
     // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // --------------- 3 sphere setup ---------------
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.50);
-    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    // auto material_left   = make_shared<dielectric>(1.50);
+    // auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
+    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
+    // ------------- many random spheres -------------
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
 
     camera cam;
 
     cam.aspect_ratio        = 16.0 / 10.0;
     cam.image_width         = 400;
-    cam.samples_per_pixel   = 100;
-    cam.max_depth           = 50;
+    cam.samples_per_pixel   = 10;
+    cam.max_depth           = 25;
 
-    cam.vfov        = 20;
-    cam.lookfrom    = point3(-2,2,1);
-    cam.lookat      = point3(0,0,-1);
-    cam.vup         = vec3(0,1,0);
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0.6;
+    cam.focus_dist    = 10.0;
 
     cam.render(world);
-
-    // // Camera
-    // auto focal_length = 1.0;
-    // auto viewport_height = 2.0;
-    // auto viewport_width = viewport_height * (double(image_width)/image_height);
-    // auto camera_center = point3(0, 0, 0);
-
-    // // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    // auto viewport_u = vec3(viewport_width, 0, 0);
-    // auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    // auto pixel_delta_u = viewport_u / image_width;
-    // auto pixel_delta_v = viewport_v / image_height;
-
-    // // Calculate the location of the upper left pixel.
-    // auto viewport_upper_left = camera_center
-    //                          - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-    // auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-
-    // std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-    // for (int j = 0; j < image_height; j++) {
-    //     std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-    //     for (int i = 0; i < image_width; i++) {
-    //         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-    //         auto ray_direction = pixel_center - camera_center;
-    //         ray r(camera_center, ray_direction);
-
-    //         color pixel_color = ray_color(r, world);
-    //         write_color(std::cout, pixel_color);
-    //     }
-    // }
-
-    // std::clog << "\rDone.                 \n";
 }
